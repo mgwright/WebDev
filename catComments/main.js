abaae23f0322894818f063de425119bca79fe989
mgwrightphysics <mgwrightphysics@gmail.com>
2018-05-11 15:15:55 -0400

    reads comments from fb

--- catComments/main.js
+++ catComments/main.js
@@ -1,25 +1,51 @@
 firebaseInit();
 
-
-
 function getPic(){
-  let url_string =  window.location.href
-  let url = new URL(url_string);
-  let id = url.searchParams.get("id");
+  let id = getId();
   
   let list = firebase.database().ref('catPics/' + id);
   list.once('value', function(snapshot) {
-    addPicToPage(snapshot.val().PictureTitle, snapshot.val().PictureLink);
+    addPicToPage(snapshot.val().PictureLink);
+    addTitleToPage(snapshot.val().PictureTitle);
   });
 }
 
-function addPicToPage(title, link){
-  
+function getComments(){
+  let id = getId();
+  let comments = firebase.database().ref('comments/' + id)
+  comments.on('child_added', function(snapshot) {
+    let c = document.createElement("li");
+    c.innerHTML = snapshot.val().comment;
+    document.getElementById("comments").appendChild(c);
+  });
+}
+
+function getId(){
+  let url_string =  window.location.href
+  let url = new URL(url_string);
+  let id = url.searchParams.get("id");
+  return id;
+}
+
+function addPicToPage(link){
   let img = document.createElement("img");
   img.src = link;
   img.width = 600;
   
   document.getElementById("pic").appendChild(img);
+}
+
+function addTitleToPage(title){
+  document.getElementById("title").innerHTML = title;
+}
+
+function onAddComment(){
+  let id = getId();
+  let comment = document.getElementById("comment").value;
+  firebase.database().ref('comments/' + id).push({
+    comment: comment 
+  });
+  document.getElementById("comment").value = "";
 }
 
 function firebaseInit() {
@@ -35,4 +61,5 @@ function firebaseInit() {
   firebase.initializeApp(config);
   console.log("Firebase is connected");
   getPic();
+  getComments();
 }
